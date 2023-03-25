9bf4c45f843ad5fb2e24017e54c4a760e940bcde
Salimhan <kurull@hotmail.com>
2023-03-26 00:41:52 +0300

    create exam page almost finished

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,40 +1,39 @@
-import Head from 'next/head';
-import { Box, Container, Stack, Typography } from '@mui/material';
-import { SettingsNotifications } from 'src/sections/settings/settings-notifications';
-import { SettingsPassword } from 'src/sections/settings/settings-password';
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import Head from "next/head";
+import { useCallback, useState } from "react";
+import { Box, Container, Stack, Typography } from "@mui/material";
+import { SettingsNotifications } from "src/sections/settings/settings-notifications";
+import { SettingsPassword } from "src/sections/settings/settings-password";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 
-const Page = () => (
-  <>
-    <Head>
-      <title>
-        Settings | Devias Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth="lg">
-        <Stack spacing={3}>
-          <Typography variant="h4">
-            Settings
-          </Typography>
-          <SettingsNotifications />
-          <SettingsPassword />
-        </Stack>
-      </Container>
-    </Box>
-  </>
-);
+const Page = () => {
+  const [questionCount, setQuestionCount] = useState(50);
 
-Page.getLayout = (page) => (
-  <DashboardLayout>
-    {page}
-  </DashboardLayout>
-);
+  return (
+    <>
+      <Head>
+        <title>Create Exam</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="lg">
+          <Stack spacing={3}>
+            <Typography variant="h4">Create Exam</Typography>
+            <SettingsNotifications
+              setQuestionCount={setQuestionCount}
+              questionCount={questionCount}
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Page;

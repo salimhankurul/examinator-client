7259662ba257ce5de86a328861801ae09176eeab
Salimhan <kurull@hotmail.com>
2023-06-02 10:42:56 +0300

    register removed

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -70,17 +70,6 @@ const Page = () => {
           <div>
             <Stack spacing={1} sx={{ mb: 3 }}>
               <Typography variant="h4">Login</Typography>
-              <Typography color="text.secondary" variant="body2">
-                Don&apos;t have an account? &nbsp;
-                <Link
-                  component={NextLink}
-                  href="/auth/register"
-                  underline="hover"
-                  variant="subtitle2"
-                >
-                  Register
-                </Link>
-              </Typography>
             </Stack>
 
             <form noValidate onSubmit={formik.handleSubmit}>
